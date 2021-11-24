a3bebc0fa4f8f28c04fba61acca5173499f66128
sepehr <sepehrhashtroudi@gmail.com>
2021-11-23 17:47:01 -0700

    evosuit script updated

--- Codec1f/evosuit_runer.sh
+++ Codec1f/evosuit_runer.sh
@@ -1,6 +1,17 @@
 mvn compile
-wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar
-wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-standalone-runtime-1.0.6.jar
+FILE="evosuite-1.0.6.jar"
+if [ -f $FILE ]; then
+    echo "$FILE exists."
+else 
+    wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-1.0.6.jar
+fi
+
+FILE="evosuite-standalone-runtime-1.0.6.jar"
+if [ -f $FILE ]; then
+    echo "$FILE exists."
+else 
+    wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-standalone-runtime-1.0.6.jar
+fi
 export EVOSUITE="java -jar $(pwd)/evosuite-1.0.6.jar"
 $EVOSUITE -class org.apache.commons.codec.StringEncoderComparator -projectCP target/classes
 mvn dependency:copy-dependencies
