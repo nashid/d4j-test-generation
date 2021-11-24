a6d443f4fc5682bc18bc3bac214c9dc9b6099205
Sepehr <sepehrhashtroudi@gmail.com>
2021-11-23 17:36:58 -0700

    evosuit script updated

--- Codec1f/evosuit_runer.sh
+++ Codec1f/evosuit_runer.sh
@@ -4,4 +4,8 @@ wget https://github.com/EvoSuite/evosuite/releases/download/v1.0.6/evosuite-stan
 export EVOSUITE="java -jar $(pwd)/evosuite-1.0.6.jar"
 $EVOSUITE -class org.apache.commons.codec.StringEncoderComparator -projectCP target/classes
 mvn dependency:copy-dependencies
-export CLASSPATH=target/classes:evosuite-standalone-runtime-1.0.6.jar:evosuite-tests:target/dependency/junit-3.8.3.jar
+export CLASSPATH=target/classes:evosuite-standalone-runtime-1.0.6.jar:evosuite-tests:target/dependency/junit-4.12.jar:target/dependency/hamcrest-core-1.3.jar
+javac evosuite-tests/org/apache/commons/codec/*.java 
+java org.junit.runner.JUnitCore org.apache.commons.codec.StringEncoderComparator_ESTest
+mvn test
+$EVOSUITE -measureCoverage -class org.apache.commons.codec.StringEncoderComparator -Djunit=org.apache.commons.codec.StringEncoderComparator_ESTest -criterion branch -projectCP target/classes:target/test-classes:evosuite-tests
